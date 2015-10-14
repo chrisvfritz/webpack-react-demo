eee0dbb9e5827e3025683d6fb072b6a815b35829
Chris Fritz <chrisvfritz@gmail.com>
2015-10-14 15:31:09 -0400

    added more info at home

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,21 @@ export default class Home extends React.Component {
   render() {
     return (
       <Layout>
-        <p>This is a static site built in JavaScript, using Webpack and React, with a lot of the heavy lifting taken care of by the hjs-webpack library. Some features of this setup include:</p>
+        <p>This is a static site built in JavaScript, using Webpack and React, with a lot of the heavy lifting taken care of by the hjs-webpack library.</p>
+        <h3>Why not?</h3>
         <ul>
-          <li>The full power of a client-side framework, with selective and customizable pre-rendering.</li>
-          <li>Live reloading <em>without state loss</em> in development.</li>
-          <li>Impossible dreams</li>
+          <li>No cool plugin ecosystem to add blog functionality, etc - you have to build everything yourself.</li>
+        </ul>
+        <h3>Why?</h3>
+        <ul>
+          <li>You get to build everything yourself!</li>
+          <li>The full power of a client-side framework, with selective, customizable, and <em>predictable</em> pre-rendering (no PhantomJS hacks!).</li>
+          <li>Live reloading in development <em>without state loss</em>.</li>
+        </ul>
+        <h3>Where?</h3>
+        <ul>
+          <li><a href='https://github.com/chrisvfritz/webpack-react-demo/'>github.com/chrisvfritz/webpack-react-demo</a></li>
+          <li><a href='http://webpack-react.surge.sh/'>webpack-react.surge.sh</a></li>
         </ul>
       </Layout>
     )
